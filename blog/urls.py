bf0747d4c772019e60990577dfe43c8dbcbd45ad
raymundodiaz <radif@Ventaja.local>
2018-01-19 20:01:27 -0600

    added views

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
